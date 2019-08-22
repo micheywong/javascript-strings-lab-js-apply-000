8469c099d0486a55a596e9b758da0b50d2aafabf
Michelle Wong <mwong@conductor.com>
2019-08-22 03:29:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson!"
 
+"Hello, " + specialGuest + "!" // "Hello, Neil deGrasse Tyson"
+
 var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "Let\'s talk about space";
